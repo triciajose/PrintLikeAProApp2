8d83bc5fc6dccc007f831c53a1a3eed142fcaf64
Andrew Wong <andrew.wong.dgnt@gmail.com>
2015-06-13 18:28:23 -0700

    database stuff

--- app/src/main/java/com/example/apps4kids/printlikeaproapp/DatabaseHandler.java
+++ app/src/main/java/com/example/apps4kids/printlikeaproapp/DatabaseHandler.java
@@ -14,116 +14,182 @@ import java.util.List;
  */
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 3;
-    private static final String DATABASE_NAME = "Test";
-    private static final String TABLE_TEST = "TestTable";
+    private static final int DATABASE_VERSION = 5;
+    private static final String DATABASE_NAME = "db";
+    private static final String PROGRESS_TABLE = "progressTable";
 
-    private static final String KEY_ID = "id";
-    private static final String KEY_NAME = "name";
-    private static final String KEY_CHECKPOINT = "checkpoint";
+    private static final String PROGRESS_COLUMN_ID = "id";
+    private static final String PROGRESS_COLUMN_NAME = "name";
+    private static final String PROGRESS_COLUMN_LETTER = "letter";
+    private static final String PROGRESS_COLUMN_LEVEL = "level";
+    private static final String PROGRESS_COLUMN_WRITTEN_NAME = "writtenName";
+    private static final String PROGRESS_COLUMN_SCORE = "score";
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     public void onCreate (SQLiteDatabase db) {
-        String CREATE_TEST_TABLE_ = "CREATE TABLE " + TABLE_TEST + "(" +
-                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_NAME + " TEXT, "
-                + KEY_CHECKPOINT + " TEXT" + ")";
-        db.execSQL(CREATE_TEST_TABLE_);
+        String createTableQuery = "CREATE TABLE " + PROGRESS_TABLE + "(" +
+                PROGRESS_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+                PROGRESS_COLUMN_NAME + " TEXT, " + 
+                PROGRESS_COLUMN_LETTER + " TEXT, " +
+                PROGRESS_COLUMN_LEVEL + " INTEGER, " +
+                PROGRESS_COLUMN_WRITTEN_NAME + " TEXT, " +
+                PROGRESS_COLUMN_SCORE + " INTEGER " +
+                ")";
+        db.execSQL(createTableQuery);
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldversion, int newversion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TEST);
+        db.execSQL("DROP TABLE IF EXISTS " + PROGRESS_TABLE);
 
         onCreate(db);
     }
 
-    // Adding new contact
-    public void addRecord(DatabaseHelper record) {
+    // Adding new user
+    public void addUser(final String username, final String writtenName) {
 
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(KEY_NAME, record.getName());
-        values.put(KEY_CHECKPOINT, record.get_checkPoint());
+        values.put(PROGRESS_COLUMN_NAME, username);
+        values.put(PROGRESS_COLUMN_WRITTEN_NAME, writtenName);
+        values.put(PROGRESS_COLUMN_LETTER, String.valueOf(writtenName.charAt(0)));
+        values.put(PROGRESS_COLUMN_LEVEL, 1);
+        values.put(PROGRESS_COLUMN_SCORE, 0);
 
         // Inserting Row
-        db.insert(TABLE_TEST, null, values);
+        db.insert(PROGRESS_TABLE, null, values);
         db.close(); // Closing database connection
     }
-    // Getting single record
-    public DatabaseHelper getRecord(int id) {
+
+
+
+    public User getUserRecord(final String username) {
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query(TABLE_TEST, new String[] { KEY_ID,
-                        KEY_NAME, KEY_CHECKPOINT }, KEY_ID + "=?",
-                new String[] { String.valueOf(id) }, null, null, null, null);
-        if (cursor != null)
-            cursor.moveToFirst();
+        Cursor cursor = db.query(PROGRESS_TABLE, new String[]{
+                        PROGRESS_COLUMN_ID,
+                        PROGRESS_COLUMN_LETTER,
+                        PROGRESS_COLUMN_LEVEL,
+                        PROGRESS_COLUMN_WRITTEN_NAME,
+                        PROGRESS_COLUMN_SCORE
+                }, PROGRESS_COLUMN_NAME + "=?",
+                new String[]{username}, null, null, null, null);
 
-        DatabaseHelper record = new DatabaseHelper(Integer.parseInt(cursor.getString(0)),
-                cursor.getString(1), cursor.getString(2));
-        // return record
-        return record;
+        User user = null;
+        if (cursor != null && cursor.moveToFirst()) {
+
+            int id = cursor.getInt(cursor.getColumnIndex(PROGRESS_COLUMN_ID));
+            char letter = cursor.getString(cursor.getColumnIndex(PROGRESS_COLUMN_LETTER)).charAt(0);
+            int level = cursor.getInt(cursor.getColumnIndex(PROGRESS_COLUMN_LEVEL));
+            String writtenName = cursor.getString(cursor.getColumnIndex(PROGRESS_COLUMN_WRITTEN_NAME));
+            int score = cursor.getInt(cursor.getColumnIndex(PROGRESS_COLUMN_SCORE));
+
+            user = new User(id, username, letter, level, writtenName, score);
+        }
+
+        cursor.close();
+        db.close();
+
+        return user;
     }
 
     // Getting All Contacts
-    public List<DatabaseHelper> getAllRecord() {
-        List<DatabaseHelper> recordList = new ArrayList<DatabaseHelper>();
-        // Select All Query
-        String selectQuery = "SELECT  * FROM " + TABLE_TEST;
+    public List<User> getAllUserNames() {
+        List<User> userList = new ArrayList<>();
+
 
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
+        Cursor cursor = db.query(PROGRESS_TABLE, new String[]{
+                        PROGRESS_COLUMN_ID,
+                        PROGRESS_COLUMN_NAME,
+                        PROGRESS_COLUMN_LETTER,
+                        PROGRESS_COLUMN_LEVEL,
+                        PROGRESS_COLUMN_WRITTEN_NAME,
+                        PROGRESS_COLUMN_SCORE
+                }, null,
+                null, null, null, PROGRESS_COLUMN_NAME + " ASC");
 
         // looping through all rows and adding to list
         if (cursor.moveToFirst()) {
             do {
-                DatabaseHelper record = new DatabaseHelper();
-                record.setID(Integer.parseInt(cursor.getString(0)));
-                record.setName(cursor.getString(1));
-                record.setcheckPoint(cursor.getString(2));
-                // Adding contact to list
-                recordList.add(record);
+                try {
+                    int id = cursor.getInt(cursor.getColumnIndex(PROGRESS_COLUMN_ID));
+                    String username = cursor.getString(cursor.getColumnIndex(PROGRESS_COLUMN_NAME));
+                    char letter = cursor.getString(cursor.getColumnIndex(PROGRESS_COLUMN_LETTER)).charAt(0);
+                    int level = cursor.getInt(cursor.getColumnIndex(PROGRESS_COLUMN_LEVEL));
+                    String writtenName = cursor.getString(cursor.getColumnIndex(PROGRESS_COLUMN_WRITTEN_NAME));
+                    int score = cursor.getInt(cursor.getColumnIndex(PROGRESS_COLUMN_SCORE));
+
+
+                    User user = new User(id, username, letter, level, writtenName, score);
+                    // Adding contact to list
+                    userList.add(user);
+                } catch (Exception e)
+                {
+
+                }
             } while (cursor.moveToNext());
         }
 
+        cursor.close();
+        db.close();
+
         // return contact list
-        return recordList;
+        return userList;
 
     }
 
     // Getting contacts Count
-    public int getRecordsCount() {
-        String countQuery = "SELECT  * FROM " + TABLE_TEST;
+    public int getUserCount() {
+        String countQuery = "SELECT  * FROM " + PROGRESS_TABLE;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(countQuery, null);
         cursor.close();
+        db.close();
 
         // return count
         return cursor.getCount();
 
     }
     // Updating single contact
-    public int updateRecord(DatabaseHelper record) {
+    public void updateUserProgress(int id, char letter, int level, int score) {
 
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(KEY_NAME, record.getName());
-        values.put(KEY_CHECKPOINT, record.get_checkPoint());
+        values.put(PROGRESS_COLUMN_LETTER, String.valueOf(letter));
+        values.put(PROGRESS_COLUMN_LEVEL, level);
+        values.put(PROGRESS_COLUMN_SCORE, score);
+
 
         // updating row
-        return db.update(TABLE_TEST, values, KEY_NAME + " = ?",
-                new String[] { String.valueOf(record.getName()) });
+        db.update(PROGRESS_TABLE, values, PROGRESS_COLUMN_ID + " = ?",
+                new String[] { String.valueOf(id) });
+        db.close();
+    }
+
+    public void updateUserName(int id, String username) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(PROGRESS_COLUMN_NAME, username);
+
+
+        // updating row
+        db.update(PROGRESS_TABLE, values, PROGRESS_COLUMN_ID + " = ?",
+                new String[] { String.valueOf(id) });
+        db.close();
     }
 
     // Deleting single contact
-    public void deleteRecord(DatabaseHelper record) {
+    public void deleteUser(String username) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_TEST, KEY_NAME + " = ?",
-                new String[] { String.valueOf(record.getName()) });
+        db.delete(PROGRESS_TABLE, PROGRESS_COLUMN_NAME + " = ?",
+                new String[] {username });
         db.close();
     }
 
