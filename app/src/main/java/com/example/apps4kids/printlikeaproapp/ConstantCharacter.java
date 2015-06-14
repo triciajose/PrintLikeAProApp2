e8122e99824132f6c6cd5d7d66b072e65bd2bbde
bigface <yaoenxin@gmail.com>
2015-06-14 08:59:11 -0700

    fix E

--- app/src/main/java/com/example/apps4kids/printlikeaproapp/ConstantCharacter.java
+++ app/src/main/java/com/example/apps4kids/printlikeaproapp/ConstantCharacter.java
@@ -30,7 +30,8 @@ public class ConstantCharacter {
         strokePath.direction = StrokeDirection.RIGHT; //First stoke of E is from left to right. The arrow should point to right.
         //Add sound effect for each stroke
         strokePath.soundUri = ""; // can link to our resource file
-        //        PATH_E.add(strokePath);
+        PATH_E.add(strokePath);
+
         StrokePath strokePath1 = new StrokePath();
         strokePath1.points.add(new Point(200, 60));
         strokePath1.points.add(new Point(200, 140));
