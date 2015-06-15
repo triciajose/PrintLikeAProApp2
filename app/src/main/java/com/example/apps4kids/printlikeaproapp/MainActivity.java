0ed232a4b16761af28f5ce45deb042c556f6fbe0
Martin Cheung <cheung.martin@gmail.com>
2015-06-14 18:48:03 -0700

    Added goodjobAnimation method in main activity and "good job" png into main layout.

--- app/src/main/java/com/example/apps4kids/printlikeaproapp/MainActivity.java
+++ app/src/main/java/com/example/apps4kids/printlikeaproapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.animation.AnimationSet;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.view.View;
@@ -124,7 +126,6 @@ public class MainActivity extends Activity {
                 startActivity(intent2);
             }
         }, (1600 * ( name.length()) + 2000 ));
-
     }
 
     @Override
@@ -149,6 +150,21 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void goodjobAnimation() {
+        ImageView imageView = (ImageView) findViewById(R.id.goodjob_iv);
+        imageView.clearAnimation();
 
+        Animation appear = AnimationUtils.loadAnimation(
+                this, R.anim.abc_slide_in_bottom);
+
+        jiggle = AnimationUtils.loadAnimation(this, R.anim.jiggle);
+
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(appear);
+        animationSet.addAnimation(jiggle);
+        animationSet.setDuration(3000);
+
+        imageView.startAnimation(animationSet);
+    }
 
 }
